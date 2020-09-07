b7a5e42d09df7242cb5462d75a65dc54ec2cc5e5
Josh Codd <jjc21@live.co.uk>
2020-09-07 22:18:10 +0100

    Improve glitch text animation and positioning.

--- components/GlitchText.jsx
+++ components/GlitchText.jsx
@@ -8,15 +8,69 @@ const GlitchContainer = styled.div`
   font-size: 100px;
 `;
 const ColourSplit = styled.span`
-  position: relative;
-  mix-blend-mode: darken;
+  position: absolute;
+  left: 0;
+  mix-blend-mode: multiply;
   color: transparent;
+`;
+
+const Hidden = styled(ColourSplit)`
+  position: relative;
+  visibility: hidden;
+`;
+
+const pinkAnimation = keyframes`
+  1% {
+    transform: translate(0px, 0px);
+  }
+  2% {
+    transform: translate(-5.5px, -2.5px) skew(5deg);
+  }
+  3% {
+    transform: translate(0px, 0px);
+  }
+  5% {
+    transform: translate(0px, 0px);
+  }
+  6% {
+    transform: translate(0px, 0px) skew(-10deg);
+  }
+  7% {
+    transform: translate(2.5px, 2.5px) ;
+  }
+  8% {
+   transform: translate(-2.5px, -2.5px) skew(-20deg);
+  }
+  9% {
+    transform: translate(-2.5px, -2.5px) skew(10deg);
+  }
+  10% {
+    transform: translate(0px, 0px);
+  }
+  13% {
+    transform: translate(0px, 0px);
+  }
+  14% {
+    transform: translate(17px, -2.5px) ;
+  }
+  15% {
+   transform: translate(-2.5px, -2.5px) skew(-10deg);
+  }
+  16% {
+   transform: translate(0px, 0px) ;
+  }
+  19% {
+   transform: translate(0px, 0px) ;
+  }
+  20% {
+    transform: translate(0px, 0px);
+  }
+  
 `;
 
 const Pink = styled(ColourSplit)`
   text-shadow: 0 0 1.5px #ff00ff;
-  transform: translate(10.5px, 2.5px);
-  ${"" /* animation: ${glitch} 1s linear infinite; */}
+  animation: ${pinkAnimation} 6s infinite;
 `;
 
 const yellowAnimation = keyframes`
@@ -69,8 +123,6 @@ const yellowAnimation = keyframes`
 `;
 
 const Yellow = styled(ColourSplit)`
-  position: absolute;
-  left: 0;
   text-shadow: 0 0 1.5px #ffff00;
   transform: translate(2.5px, 2.5px);
   animation: ${yellowAnimation} 5s infinite;
@@ -126,29 +178,22 @@ const blueAnimation = keyframes`
 `;
 
 const Blue = styled(ColourSplit)`
-  position: absolute;
-  left: 0;
   text-shadow: 0 0 1.5px #00ffff;
   transform: translate(-2.5px, -2.5px);
   animation: ${blueAnimation} 6s infinite;
-`;
-
-const Black = styled(ColourSplit)`
-  position: absolute;
-  left: 0;
-  text-shadow: 0 0 1px black;
-  opacity: 0.1;
-  animation: ${yellowAnimation} 6s infinite;
 `;
 
 function GlitchText(props) {
   const text = props.text;
   return (
     <GlitchContainer>
-      <Pink>{text}</Pink>
-      <Yellow>{text}</Yellow>
+      <Hidden>{text}</Hidden>
       <Blue>{text}</Blue>
-      <Black>{text}</Black>
+      <Yellow>{text}</Yellow>
+      <Pink>{text}</Pink>
+
+      <Blue>{text}</Blue>
+      <Yellow>{text}</Yellow>
     </GlitchContainer>
   );
 }
