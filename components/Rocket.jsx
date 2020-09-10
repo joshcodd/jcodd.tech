0aa0a1dbe834290e98b34c9ea0b3e0e3c9df842b
Josh Codd <jjc21@live.co.uk>
2020-09-10 15:48:05 +0100

    Change rocket Animation

--- components/Rocket.jsx
+++ components/Rocket.jsx
@@ -17,49 +17,59 @@ const RocketImage = styled.img`
   width: 100%;
 `;
 
-const boosterAnimation = keyframes`
+const boosterAnimationTop = keyframes`
 
   0% {
-    transform: skew(3deg) translateX(6.5px) scaleX(-1)  ;
+    transform: skew(7deg) translateX(5.3%) scaleY(0.8) translateY(-10%) ;
   }
-  20% {
-    transform: skew(-3deg) translateX(-6.5px) scaleX(-1) ;
-
-  }
-  40% {
-    transform: skew(3deg) translateX(6.5px) scaleX(-1) ;
-  }
-  60% {
-    transform: skew(3deg) translateX(6.5px) scaleX(1) ;
-  }
-  80% {
-    transform: skew(-3deg) translateX(-6.5px) scaleX(1) ;
+  50% {
+    transform: skew(-7deg) translateX(-5.7%) scaleY(0.8) translateY(-10%) ;
 
   }
   100% {
-    transform: skew(3deg) translateX(6.5px) scaleX(-1) ;
+    transform: skew(7deg) translateX(5.3%) scaleY(0.8) translateY(-10%);
+  }
+`;
+
+const boosterAnimationBottom = keyframes`
+
+0% {
+    transform: skew(7deg) translateX(5.3%) scaleY(0.6) translateY(-20.8%)  ;
+  }
+  50% {
+    transform: skew(-7deg) translateX(-5.7%) scaleY(0.75) translateY(-12.1%)   ;
+
+  }
+  100% {
+    transform: skew(7deg) translateX(5.3%) scaleY(0.6) translateY(-20.8%);
   }
 
 `;
 
-const BoosterImage = styled.img`
+const BoosterImageTop = styled.img`
   position: absolute;
   top: 82.5%;
   left: 0px;
   right: 0px;
   height: auto;
   width: 100%;
+  transform: scaleY(0.75) translateY(-13%);
+  clip-path: inset(9% 0% 72% 0px);
+  opacity: 0.3;
+  animation: ${boosterAnimationTop} 0.7s infinite;
+`;
 
-  clip-path: inset(9% 0px 0px 0px);
-  opacity: 0.5;
-  animation: ${boosterAnimation} 0.3s infinite;
+const BoosterImageBottom = styled(BoosterImageTop)`
+  clip-path: inset(28% 0px 0px 0px);
+  animation: ${boosterAnimationBottom} 0.7s infinite;
 `;
 
 function Sphere() {
   return (
     <RocketContainer>
       <RocketImage src="rocket-size.svg" />
-      <BoosterImage src="rocket-short.svg" />
+      <BoosterImageTop src="rocket-short.svg" />
+      <BoosterImageBottom src="rocket-short.svg" />
     </RocketContainer>
   );
 }
