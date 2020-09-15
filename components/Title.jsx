cc766a4b06cc75af4f6233d454607f1ffcae7bcd
Josh Codd <jjc21@live.co.uk>
2020-09-15 22:52:31 +0100

    Change mobile width

--- components/Title.jsx
+++ components/Title.jsx
@@ -94,7 +94,7 @@ function Title() {
     <TitleContainer>
       <LeftContainer>
         <TextContainer>
-          <GlitchText text="Hi, I'm Josh" />
+          <GlitchText text="Hi, I'mm Josh" />
 
           <ImAText>
             I'm a software engineer based in Swansea, Wales.
