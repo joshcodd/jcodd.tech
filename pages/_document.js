04eb8bf4cf3d2cc84fffadf686c41a3d1ca41f15
Josh Codd <jjc21@live.co.uk>
2020-09-22 19:49:12 +0100

    Add lang to HTML

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
