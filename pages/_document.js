322cbd9e0a3ffe61e4335eee0b3ba6af32a612f9
Josh Codd <jjc21@live.co.uk>
2020-09-23 17:41:15 +0100

    Fix FOUC

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
+  //https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+  //Fix FOUC
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
