c7514129c67bfd878f117ce2ceb3ca0b091edf3e
Josh Codd <jjc21@live.co.uk>
2020-09-24 03:09:26 +0100

    Change mobile transition

--- components/SpaceScene.jsx
+++ components/SpaceScene.jsx
@@ -46,7 +46,7 @@ const SpaceItem = styled.img`
   animation: ${spaceEntry} 0.5s;
 
   @media (max-width: 500px) {
-    transition: transform 0.11s linear;
+    transition: transform 0.15s linear;
   }
 `;
 
@@ -113,10 +113,10 @@ function SpaceScene(props) {
   const [parallax, setParallax] = useState(null);
 
   useEffect(() => {
-    window.addEventListener("scroll", throttle(handleParallax, 100));
+    window.addEventListener("scroll", throttle(handleParallax, 90));
 
     return () => {
-      window.removeEventListener("scroll", throttle(handleParallax, 100));
+      window.removeEventListener("scroll", throttle(handleParallax, 90));
     };
   }, []);
 
